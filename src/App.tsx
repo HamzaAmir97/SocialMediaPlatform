a3af14808a490fc80a73a33001205fc1b438a056
Hmazah <alhamza2012@gmail.com>
2024-08-05 10:00:00 +0300

    add  routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
-import { useState } from 'react'
 
 import './App.css'
 import { Routes,Route } from "react-router-dom";
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import { Home } from './root/pages';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './root/RootLayout';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <main className='flex h-screen w-screen'>
+
     <Routes>
+    <Route element ={<AuthLayout/>}>
+
+    <Route path='/sign-in' element={<SigninForm />} />
+    <Route path='/sign-up' element={<SignupForm />} />
+    
+  
+   </Route>
+
 
    {/* //public routes */}
-    <Route path='/sign-in' element={<SigninForm />} />
-    
+   
 {/* private routes */}
+    <Route element={<RootLayout/>}>
     <Route index element={<Home/>} />
-
+    </Route>
 
     </Routes>
     </main>
