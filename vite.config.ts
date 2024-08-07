e2ed6b8912e797e1fd8de23eb26fc60cdd2d7c5e
Hmazah <alhamza2012@gmail.com>
2024-08-07 10:00:00 +0300

    add shadcn compo

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,16 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [require('tailwindcss'), require('autoprefixer')],
     },
   },
 })
