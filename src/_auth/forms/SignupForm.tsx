de4e437d0b62dad5875e4ff4980525aa3762ac68
Hmazah <alhamza2012@gmail.com>
2024-08-18 10:00:00 +0300

    add  button

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,6 +1,33 @@
+import { z } from "zod"
 import { Button } from "@/components/ui/button"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
 
-function SignupForm() {
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+})
+
+
+
+const SignupForm = () => {
+
+// 1. Define your form.
+const form = useForm<z.infer<typeof formSchema>>({
+  resolver: zodResolver(formSchema),
+  defaultValues: {
+    username: "",
+  },
+})
+
+// 2. Define a submit handler.
+function onSubmit(values: z.infer<typeof formSchema>) {
+  // Do something with the form values.
+  // ✅ This will be type-safe and validated.
+  console.log(values)
+}
+
+
+
   return (
     <div>
       
