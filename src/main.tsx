65134be8b75eeacd7be3a700b6f27cddb53a46c3
Hmazah <alhamza2012@gmail.com>
2024-09-02 10:00:00 +0300

    add  tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from "@/context/AuthContext";
+import { QueryProvider } from "@/lib/react-query/QueryProvider";
 
-createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App /> 
-  </BrowserRouter>,
-)
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <QueryProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
