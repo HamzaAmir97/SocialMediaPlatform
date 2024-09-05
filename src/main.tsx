c4dd358ea1a9c80c2d5900785461554514bc1cf1
Hmazah <alhamza2012@gmail.com>
2024-09-05 10:00:00 +0300

    fix the authcontext file

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { QueryProvider } from "@/lib/react-query/QueryProvider";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  
     <BrowserRouter>
       <QueryProvider>
         <AuthProvider>
@@ -16,5 +16,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         </AuthProvider>
       </QueryProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  
 );
