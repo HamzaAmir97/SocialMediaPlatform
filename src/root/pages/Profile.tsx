0b8145ec5e2efe9e0684efe5658789fd9fd0703e
Hmazah <alhamza2012@gmail.com>
2024-11-05 10:00:00 +0200

    edit profile

--- src/root/pages/Profile.tsx
+++ src/root/pages/Profile.tsx
@@ -1,3 +1,6 @@
+import Loader from "@/components/ui/shared/Loader";
+import { useUserContext } from "@/context/AuthContext";
+import { useGetUserById } from "@/lib/react-query/queriesAndMutations";
 import {
   Route,
   Routes,
@@ -7,11 +10,7 @@ import {
   useLocation,
 } from "react-router-dom";
 
-import { Button } from "@/components/ui";
-import { LikedPosts } from "@/_root/pages";
-import { useUserContext } from "@/context/AuthContext";
-import { useGetUserById } from "@/lib/react-query/queries";
-import { GridPostList, Loader } from "@/components/shared";
+
 
 interface StabBlockProps {
   value: string | number;
