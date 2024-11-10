68af220169bdf4a8788753b679f2f7420aa7810b
Hmazah <alhamza2012@gmail.com>
2024-11-10 10:00:00 +0200

    add user card

--- src/components/ui/shared/UserCard.tsx
+++ src/components/ui/shared/UserCard.tsx
@@ -1,7 +1,8 @@
 import { Models } from "appwrite";
 import { Link } from "react-router-dom";
+import { Button } from "../button";
 
-import { Button } from "../ui/button";
+
 
 type UserCardProps = {
   user: Models.Document;
