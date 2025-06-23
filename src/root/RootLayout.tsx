b6c7d45a4bea6a2e243614b5571bf305cc6cd91b
Hmazah <alhamza2012@gmail.com>
2025-06-23 12:00:00 +0300

    final commit  add readme file

--- src/root/RootLayout.tsx
+++ src/root/RootLayout.tsx
@@ -1,10 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 import Topbar from "@/components/ui/shared/Topbar";
 import Bottombar from "@/components/ui/shared/Bottombar";
 import LeftSidebar from "@/components/ui/shared/LeftSidebar";
+import { useUserContext } from "@/context/AuthContext";
 
 const RootLayout = () => {
+  const { user } = useUserContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/sign-in"); // أو أي مسار لصفحة تسجيل الدخول
+    }
+  }, [user, navigate]);
+
+  if (!user) {
+    return null; // انتظر التوجيه
+  }
+
   return (
     <div className="w-full md:flex">
       <Topbar />
